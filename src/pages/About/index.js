e0b0184dcd86a818da1bad3b3030f22102e856e1
Ítalo Rizzon <123012256+IRizzon@users.noreply.github.com>
2025-07-09 17:12:52 -0300

    bio iniciada
    adicionado fonte Archivo Black e bio iniciada

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,6 +1,21 @@
+import './about.css';
+
+import truckTitle from '../../assets/image/history/truckTitle.jfif';
+
 function About(){
     return(
-        <h1>Sobre Nós</h1>
+        <div className='about'>
+            <div className='cardTitle'>
+                <img src={truckTitle} className='bgTitle'/>
+                <div className='bgOverlay'/>
+                <div className='aTitle'>
+                    <h1>JM Precisão</h1>
+                    <h2>De Coromandel para todo o Brasil</h2>
+                </div>
+            </div>
+
+
+        </div>
     )
 }
 
