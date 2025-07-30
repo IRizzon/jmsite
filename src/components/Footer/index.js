9d530120617ca0b16f1085254d1e3d4095733d2f
Ítalo Rizzon <123012256+IRizzon@users.noreply.github.com>
2025-07-30 11:04:50 -0300

    link's
    link adicionado ao endereço e às redes sociais

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -11,26 +11,42 @@ function Footer(){
         <footer>
             <div className='addcontainer'>
                 <FaMapMarkerAlt className='icon'/>
-                <div className='address'>
+                <Link 
+                    className='address'
+                    to='https://maps.app.goo.gl/ebPrDuuirxTZRKyR6'
+                    target='_blank'
+                >
                     <h3>Rua Vicente Valadares, 237</h3>
                     <h3>Distrito Industrial - 38551-022</h3>
                     <h2>Coromandel - MG</h2>
-                </div>
+                </Link>
             </div>
             <Link className='logo' to="/"><img id='logofjm' src={logofantasia} alt='Logo JMPrecisão'/></Link>
             <div className='social'>
-                <div>
-                    <FaWhatsapp className='icon'/>
+                <Link 
+                    to='https://wa.me/5534999257079' 
+                    target='_blank'
+                    className='social_Link'
+                >
+                    <FaWhatsapp className='icon' />
                     <h2>(34) 9 9925-7079</h2>
-                </div>
-                <div>
-                    <FaWhatsapp className='icon'/>
+                </Link>
+                <Link 
+                    to='https://wa.me/5534988074756' 
+                    target='_blank'
+                    className='social_Link'
+                >
+                    <FaWhatsapp className='icon' />
                     <h2>(34) 9 8807-4756</h2>
-                </div>
-                <div>
-                    <FaInstagram className='icon'/>
+                </Link>
+                <Link 
+                    to='httpe://https://www.instagram.com/jm.precisao/' 
+                    target='_blank'
+                    className='social_Link'
+                >
+                    <FaInstagram  className='icon' />
                     <h2>@jm.precisao</h2>
-                </div>
+                </Link>
             </div>
         </footer>
     )
